3bf282325435354b087a0241c5e46b56353c94bf
Luke Raimondo Moia <pe75yel@eduvaud.ch>
2024-08-30 12:12:49 +0200

    Fin 1er Semaine

--- Personnel/NCUD/Program.cs
+++ Personnel/NCUD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,40 @@ namespace NCUD
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string directory = Directory.GetCurrentDirectory() + "..\\..\\..\\";
+                // Console.WriteLine(Directory.GetCurrentDirectory());
+                string[] dirs = Directory.GetDirectories(directory, "*");
+                string[] files = Directory.GetFiles(directory, "*");
+                // IEnumerable<string> dirs = Directory.EnumerateDirectories(directory);
+
+
+                // files
+
+                foreach (string file in files)
+                {
+                    // Console.WriteLine(dir);
+                    string splt = Path.GetFileName(file);
+                    FileInfo fi = new FileInfo(file);
+                    Console.WriteLine(splt);
+                    Console.WriteLine(fi.Length);
+                }
+
+                // directory
+
+                foreach (string dir in dirs)
+                {
+                    // Console.WriteLine(dir);
+                    string splt = Path.GetFileName(dir);
+                    Console.WriteLine(splt);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            Console.ReadLine();
         }
     }
 }
